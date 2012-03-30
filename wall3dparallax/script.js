ffe941e256125e63c2cd55342caebfac16c11e30
felquis <ofelquis@hotmail.com>
2012-03-30 02:14:29 -0300

    Adicionada mouseMoveTransform e melhorada algumas linhas

--- wall3dparallax/script.js
+++ wall3dparallax/script.js
@@ -4,14 +4,40 @@
 	Blog   : tutsmais.com.br
 */
 !(function ($doc, $win) {
-	
-		$imgs = $doc.getElementsByTagName('img'),
-		totalImg = $imgs.length - 1;
+	/*
+		Faz as imagens aparecerem com um fade maneiro ;)
+	*/
+var $imgs = $doc.querySelectorAll('img'),
+	totalImg = $imgs.length - 1,
+	screenWidth = $win.screen.width / 2,
+	screenHeight = $win.screen.height / 2,
+	validPropertyPrefix = '',
+	otherProperty = 'perspective(600px)';
 
-		for (var i = totalImg; i>=0; i--) {
-			$imgs[i].onload = function () {
-				this.style.opacity = 1;
-			}
+// Faz as imagens aparecerem com uma opacidade
+	for (var i = totalImg; i>=0; i--) {
+		$imgs[i].onload = function () {
+			this.style.opacity = 1;
 		}
-	
+	}
+
+// Verifica qual propriedade usar no browsers 
+	if(typeof $imgs[0].style.webkitTransform == 'string') {
+		validPropertyPrefix = 'webkitTransform';
+	} else {
+		validPropertyPrefix = 'MozTransform';
+	}
+
+// Adiciona o evento de mousemove ao documento
+	$doc.addEventListener('mousemove', function (e) {
+		// Calcula o meio de x, y da tela
+		var centroX 	= e.clientX - screenWidth,
+			centroY 	= screenHeight - (e.clientY + 13),
+			degX   = centroX * 0.1,
+			degY   = centroY * 0.1
+	// Percorre os elementos adicionando as transformações
+		for (var i = totalImg; i>=0; i--) {
+			$imgs[i].parentNode.style[validPropertyPrefix] = otherProperty + 'rotateY('+ degX +'deg)  rotateX('+ degY +'deg)';
+		}
+	});
 })(document, window);
