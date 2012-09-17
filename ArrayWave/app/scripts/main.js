206d6c8c4d42d508faf682c262dfb2090ff44909
felquis <ofelquis@hotmail.com>
2012-09-17 13:57:13 -0300

    Criada representacao do DOM em uma matriz ;)

--- ArrayWave/app/scripts/main.js
+++ ArrayWave/app/scripts/main.js
@@ -8,6 +8,8 @@
 
 			$canvas = $('#canvas'),
 
+			MatrizDOM = [],
+
 		// Função que retorna um HTML que representa uma matriz de array
 			toHTML = function (matriz) {
 				var html = [];
@@ -31,7 +33,27 @@
 
 		$canvas.html(toHTML(Matriz));
 
+	// Cria uma matriz que representa a matriz em questão só que no DOM
 
+	(function () {
+		//	MatrizDOM
+
+		$canvas.find('.line').each(function (i, e) {
+		// Cada coluna e seus itens são instancias do DOM
+			var colunas = (function () {
+				var col = [];
+
+				$(e).find('span').each(function (i, e) {
+					col.push($(this));
+				});
+
+				return col;
+			}());
+
+		// Marca uma linha
+			MatrizDOM.push(colunas);
+		});
+	}());
 
 	// Hover nos span, retorna a posição do elemento no Array
 		$canvas.on('hover', 'span', function () {
@@ -39,7 +61,7 @@
 				line = $this.parent().index(),
 				col = $this.index();
 
-			console.log(line, col);
+			MatrizDOM[line][col].html('-');
 		});
 	});
 }(jQuery, window, document));
