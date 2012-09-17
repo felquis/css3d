979c5b0ea83de5c410ffb9404ae0d55cc68a24f0
felquis <ofelquis@hotmail.com>
2012-09-17 13:34:06 -0300

    Funcao que transforma Array em HTML

--- ArrayWave/app/scripts/main.js
+++ ArrayWave/app/scripts/main.js
@@ -1,5 +1,29 @@
 ;(function ($, window, document, undefined) {
 	$(function () {
-		console.log('Wow! Parece estar tudo pronto por aqui.');
+		var Matriz = [
+						[1, 2, 3],
+						[1, 2, 3],
+						[1, 2, 3]
+					],
+			html = [],
+			$canvas = $('#canvas');
+
+		html.push('<div class="principal">');
+
+		$.each(Matriz, function (i, e) {
+				if ($.isArray(e)) {
+					html.push('<div class="line">');
+						$.each(e, function (i, e) {
+							html.push('<span>'+e+'</span>');
+						});
+					html.push('</div>');
+				}
+		});
+
+		html.push('</div>');
+
+		$canvas.html(html.join(''));
+
+		console.log(html.join(''))
 	});
 }(jQuery, window, document));
